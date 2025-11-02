054726ec86ba4e413d3aa1f14c599474b4cde9df
RomanRomanov998 <romanovramik@gmail.com>
2025-11-02 22:09:24 +0100

    Remove constructor from BackgroundModule

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -2,10 +2,6 @@ import { Module } from "../core/module";
 import { random } from "../utils";
 
 export class BackgroundModule extends Module {
-  constructor() {
-    super("background", "Случайный фон");
-  }
-
   trigger() {
     document.body.style.backgroundColor = `rgb(${random(0, 255)}, ${random(
       0,
