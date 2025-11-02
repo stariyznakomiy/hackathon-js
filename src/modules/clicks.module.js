41d0d040d3e9129329e6321e5ebc618bd01b6b1d
RomanRomanov998 <romanovramik@gmail.com>
2025-11-02 22:11:20 +0100

    Removed constructors from modules, cleaned code

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,30 @@
 import { Module } from "../core/module";
 
 export class ClicksModule extends Module {
-  trigger() {}
+  trigger() {
+    alert("Таймер запущен (5 сек)! Делайте клики!");
+
+    let counterAllClicks = -1;
+    let counterDoubleClicks = 0;
+
+    const click1Handler = () => {
+      counterAllClicks++;
+    };
+
+    const click2Handler = () => {
+      counterDoubleClicks++;
+    };
+
+    document.addEventListener("click", click1Handler);
+    document.addEventListener("dblclick", click2Handler);
+
+    setTimeout(() => {
+      document.removeEventListener("click", click1Handler);
+      document.removeEventListener("dblclick", click2Handler);
+
+      alert(
+        `Всего кликов - ${counterAllClicks}, в том числе ${counterDoubleClicks} двойных!`
+      );
+    }, 5000);
+  }
 }
